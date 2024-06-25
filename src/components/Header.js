c6af145fedffd4e580373fc7c6f1203f23897a64
busrabll <busrabal.802@gmail.com>
2024-06-25 14:25:05 +0300

    25.06.24

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import bootstrap from 'bootstrap'
+import { BsSearch } from 'react-icons/bs';
 
 const Header = () => {
     return (
@@ -9,15 +11,116 @@ const Header = () => {
                     <div className='row'>
                         <div className='col-6'>
                             <p className='text-white mb-0'>
-                                Free Shipping $100 & Free Returns
+                                Free shipping over 100 returns
                             </p>
                         </div>
                         <div className='col-6'>
                             <p className='text-end text-white mb-0'>
-                                Hotline: <a href="tel: +90 5331609629">
-                                    +90 5331609629
-                                </a>
+                                Hotline: <a className='text-white' href='+90 5331609629'>+90 5331609629</a>
                             </p>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className='header-upper py-3'>
+                <div className="container-xxl">
+                    <div className="row align-item-center">
+                        <div className="col-2">
+                            <h2><Link className='text-white'>Dev Corner</Link></h2>
+                        </div>
+                        <div className="col-5">
+                            <div className="input-group">
+                                <input type="text" className="form-control py-2" placeholder="Search Product Here..." aria-label="Search Product Here..." aria-describedby="basic-addon2" />
+                                <span className="input-group-text p-3" id="basic-addon2">
+                                    <BsSearch className='fs-6' />
+                                </span>
+                            </div>
+                        </div>
+                        <div className="col-5">
+                            <div className="header-upper-links d-flex align-items-center justify-content-between">
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src="images/compare.png" alt="compare" />
+                                        <p className='mb-0'>
+                                            compare <br /> Products
+                                        </p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src="images/wishlist.png" alt="wishlist" />
+                                        <p className='mb-0'>
+                                            Favoruite <br /> Wishlist
+                                        </p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src="images/user.png" alt="user" />
+                                        <p className='mb-0'>
+                                            Log in <br /> My Account
+                                        </p>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link className='d-flex align-items-center gap-10 text-white'>
+                                        <img src="images/cart.png" alt="cart" />
+                                        <div className='d-flex flex-column gap-10'>
+                                            <span className="badge bg-white text-dark">0</span>
+                                            <p className='mb-0'>$500</p>
+                                        </div>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <header className="header-bottom py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="menu-bottom d-flex align-items-center gap-30">
+                                <div>
+                                    <div className="dropdown">
+                                        <button className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex align-items-center" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <img src="images/menu.png" alt="" />
+                                            <span className='me-5 d-inline-block'>Shop Categories</span>
+                                        </button>
+                                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                            <li>
+                                                <Link className="dropdown-item text-white" to="">
+                                                    Action
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link className="dropdown-item text-white" to="">
+                                                    Another action
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link className="dropdown-item text-white" to="">
+                                                    Something else here
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className='menu-links'>
+                                    <div className="menu-item">
+                                        <NavLink to="/">Home</NavLink>
+                                    </div>
+                                    <div className="menu-item">
+                                        <NavLink to="/">Our Store</NavLink>
+                                    </div>
+                                    <div className="menu-item">
+                                        <NavLink to="/">Blogs</NavLink>
+                                    </div>
+                                    <div className="menu-item">
+                                        <NavLink to="/contact">Contact</NavLink>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -26,4 +129,4 @@ const Header = () => {
     );
 }
 
-export default Header
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,88 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-l py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative p-3">
+                <img src="images/main-banner.png"
+                  className='img-fluid rounded-3'
+                  alt="main banner"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUNNY DAYS</h4>
+                  <h5>Vogue</h5>
+                  <p>
+                    Prices starting from 2000 TL
+                  </p>
+                  <Link className='button'>BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative p-3">
+                  <img src="images/main-banner.png"
+                    className='img-fluid rounded-3'
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUNNY DAYS</h4>
+                    <h5>Vogue</h5>
+                    <p>
+                      Prices starting from 2000 TL
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img src="images/main-banner.png"
+                    className='img-fluid rounded-3'
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUNNY DAYS</h4>
+                    <h5>Vogue</h5>
+                    <p>
+                      Prices starting from 2000 TL
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img src="images/main-banner.png"
+                    className='img-fluid rounded-3'
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUNNY DAYS</h4>
+                    <h5>Vogue</h5>
+                    <p>
+                      Prices starting from 2000 TL
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img src="images/main-banner.png"
+                    className='img-fluid rounded-3'
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUNNY DAYS</h4>
+                    <h5>Vogue</h5>
+                    <p>
+                      Prices starting from 2000 TL
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section >
+    </>
   )
 }
 
